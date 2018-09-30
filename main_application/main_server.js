a6ee21090fbf35d25c99be3d133eca4f79cccae8
jess7979 <theoneandtheonlyjess@gmail.com>
2018-09-29 20:14:17 -0400

    main_server receive request and print body

--- main_application/main_server.js
+++ main_application/main_server.js
@@ -1,5 +1,5 @@
 var http = require('http');
-var EventEmitter = require('events');
+/*var EventEmitter = require('events');
 
 class MyEmitter extends EventEmitter {};
 
@@ -19,7 +19,13 @@ myEmitter.on('event', function(a, b) {
 
 });
 
+    buffer = {
+    "key" : "value",
+    "key2" : "val2"
+};
+*/
 http.createServer((request, response) => {
+
     const { headers, method, url } = request;
     let body = [];
     request.on('error', (err) => {
@@ -28,12 +34,32 @@ http.createServer((request, response) => {
         body.push(chunk);
     }).on('end', () => {
         body = Buffer.concat(body).toString();
-        // At this point, we have the headers, method, url and body, and can now
-        // do whatever we need to in order to respond to this request.
+        // BEGINNING OF NEW STUFF
 
-        console.log(headers);
-        console.log(body);
-        response.writeHead(200);
-        response.end()
+        response.on('error', (err) => {
+            console.error(err);
+        });
+
+        response.statusCode = 200;
+        response.setHeader('Content-Type', 'application/json');
+        // Note: the 2 lines above could be replaced with this next one:
+        // response.writeHead(200, {'Content-Type': 'application/json'})
+
+        const responseBody = { headers, method, url, body };
+
+        let str = JSON.stringify(responseBody.body)
+
+
+        console.log(str.toString())
+       // console.log(JSON.stringify(responseBody.body));
+        response.write(JSON.stringify(responseBody.body));
+
+
+
+        response.end();
+        // Note: the 2 lines above could be replaced with this next one:
+        // response.end(JSON.stringify(responseBody))
+
+        // END OF NEW STUFF
     });
 }).listen(8080); // Activates this server, listening on port 8080.
